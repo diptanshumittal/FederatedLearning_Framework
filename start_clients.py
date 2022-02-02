feebfae44d08cd4bda9b4c2cfa6e73d790cb95a0
diptanshumittal <diptanshu18232@iiitd.ac.in>
2022-02-02 16:08:15 +0100

    Added SGD optimizer

--- start_clients.py
+++ start_clients.py
@@ -50,8 +50,8 @@ def start_clients_docker():
 
 def start_clients():
     try:
-        available_gpus = ["cuda:0", "cuda:0", "cuda:2", "cuda:3", "cuda:0", "cuda:1", "cuda:2", "cuda:3"]
-        for i in range(1, 3):
+        available_gpus = ["cuda:0", "cuda:1", "cuda:2", "cuda:3", "cuda:0", "cuda:1", "cuda:2", "cuda:3"]
+        for i in range(1, 2):
             Process(target=run_container,
                     args=("python Client/client.py --gpu=" + available_gpus[i - 1] + " --client_id=" + str(i),),
                     daemon=True).start()
