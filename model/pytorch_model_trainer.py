3f94f7969a839d3f09226e9d137faf54d0fd3ee7
diptanshumittal <diptanshu18232@iiitd.ac.in>
2022-02-08 12:20:48 +0530

    Added paper implementation

--- model/pytorch_model_trainer.py
+++ model/pytorch_model_trainer.py
@@ -6,6 +6,7 @@ import yaml
 import torch
 import threading
 import collections
+import time
 from torch.utils.data import DataLoader
 from helper.pytorch.pytorch_helper import PytorchHelper
 from helper.pytorch.lr_scheduler import CosineAnnealingLR, MultiStepLR
@@ -41,6 +42,7 @@ class PytorchModelTrainer:
         # self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
         self.device = torch.device(config["cuda_device"])
         self.loss = self.loss.to(self.device)
+        self.model.to(self.device)
         args = {
             "warm_up_epochs": int(config["model"]["warm_up_epochs"]),
             "epochs": int(config["model"]["epochs"]),
@@ -49,12 +51,12 @@ class PytorchModelTrainer:
             "lr": float(config["model"]["learning_rate"]),
             "lrmilestone": list(map(int, config["model"]["lrmilestone"].split(" ")))
         }
-        self.scheduler = MultiStepLR(self.optimizer, args)
+        self.scheduler = torch.optim.lr_scheduler.MultiStepLR(self.optimizer, milestones=args["lrmilestone"], last_epoch=-1)
         print("Device being used for training :", self.device, flush=True)
         self.train_loader = DataLoader(self.helper.read_data(config["dataset"], config["data_path"], True),
-                                       batch_size=int(config['batch_size']), shuffle=True)
+                                       batch_size=int(config['batch_size']), shuffle=True, pin_memory=True)
         self.test_loader = DataLoader(self.helper.read_data(config["dataset"], config["data_path"], False),
-                                      batch_size=int(config['batch_size']), shuffle=True)
+                                      batch_size=int(config['batch_size']), shuffle=True, pin_memory=True)
 
     def evaluate(self, dataloader):
         self.model.eval()
@@ -128,6 +130,128 @@ class PytorchModelTrainer:
             return {"status": "fail"}
 
 
+def train(train_loader, model, criterion, optimizer, epoch):
+    """
+        Run one train epoch
+    """
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    # switch to train mode
+    model.train()
+    end = time.time()
+    for i, (input, target) in enumerate(train_loader):
+        # measure data loading time
+        data_time.update(time.time() - end)
+        target = target.cuda()
+        input_var = input.cuda()
+        target_var = target
+        # compute output
+        output = model(input_var)
+        loss = criterion(output, target_var)
+        # compute gradient and do SGD step
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+        output = output.float()
+        loss = loss.float()
+        # measure accuracy and record loss
+        prec1 = accuracy(output.data, target)[0]
+        losses.update(loss.item(), input.size(0))
+        top1.update(prec1.item(), input.size(0))
+        # measure elapsed time
+        batch_time.update(time.time() - end)
+        end = time.time()
+        if i % 50 == 0:
+            print('Epoch: [{0}][{1}/{2}]\t'
+                  'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                  'Data {data_time.val:.3f} ({data_time.avg:.3f})\t'
+                  'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                  'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                epoch, i, len(train_loader), batch_time=batch_time, data_time=data_time, loss=losses, top1=top1))
+
+
+def validate(val_loader, model, criterion):
+    """
+    Run evaluation
+    """
+    batch_time = AverageMeter()
+    losses = AverageMeter()
+    top1 = AverageMeter()
+    # switch to evaluate mode
+    model.eval()
+    end = time.time()
+    with torch.no_grad():
+        for i, (input, target) in enumerate(val_loader):
+            target = target.cuda()
+            input_var = input.cuda()
+            target_var = target.cuda()
+            # compute output
+            output = model(input_var)
+            loss = criterion(output, target_var)
+            output = output.float()
+            loss = loss.float()
+            # measure accuracy and record loss
+            prec1 = accuracy(output.data, target)[0]
+            losses.update(loss.item(), input.size(0))
+            top1.update(prec1.item(), input.size(0))
+            # measure elapsed time
+            batch_time.update(time.time() - end)
+            end = time.time()
+            if i % 50 == 0:
+                print('Test: [{0}/{1}]\t'
+                      'Time {batch_time.val:.3f} ({batch_time.avg:.3f})\t'
+                      'Loss {loss.val:.4f} ({loss.avg:.4f})\t'
+                      'Prec@1 {top1.val:.3f} ({top1.avg:.3f})'.format(
+                    i, len(val_loader), batch_time=batch_time, loss=losses,
+                    top1=top1))
+
+    print(' * Prec@1 {top1.avg:.3f}'
+          .format(top1=top1))
+    return top1.avg
+
+
+def save_checkpoint(state, is_best, filename='checkpoint.pth.tar'):
+    """
+    Save the training model
+    """
+    torch.save(state, filename)
+
+
+class AverageMeter(object):
+    """Computes and stores the average and current value"""
+
+    def __init__(self):
+        self.reset()
+
+    def reset(self):
+        self.val = 0
+        self.avg = 0
+        self.sum = 0
+        self.count = 0
+
+    def update(self, val, n=1):
+        self.val = val
+        self.sum += val * n
+        self.count += n
+        self.avg = self.sum / self.count
+
+
+def accuracy(output, target, topk=(1,)):
+    """Computes the precision@k for the specified values of k"""
+    maxk = max(topk)
+    batch_size = target.size(0)
+    _, pred = output.topk(maxk, 1, True, True)
+    pred = pred.t()
+    correct = pred.eq(target.view(1, -1).expand_as(pred))
+    res = []
+    for k in topk:
+        correct_k = correct[:k].view(-1).float().sum(0)
+        res.append(correct_k.mul_(100.0 / batch_size))
+    return res
+
+
 if __name__ == "__main__":
     with open('settings/settings-common.yaml', 'r') as file:
         try:
@@ -146,8 +270,8 @@ if __name__ == "__main__":
     model_trainer.model.to(model_trainer.device)
     stop_round_event = threading.Event()
     model_trainer.stop_event = stop_round_event
-    for i in range(30):
-        print(i)
-        model_trainer.train({"epochs": 10})
-        print("After epochs", str((i + 1) * 10), "on device", client_config["training"]["cuda_device"], "results",
-              model_trainer.validate())
+    for i in range(2):
+        print('current lr {:.5e}'.format(model_trainer.optimizer.param_groups[0]['lr']))
+        train(model_trainer.train_loader, model_trainer.model, model_trainer.loss, model_trainer.optimizer, i + 1)
+        model_trainer.scheduler.step()
+        validate(model_trainer.test_loader, model_trainer.model, model_trainer.loss)
